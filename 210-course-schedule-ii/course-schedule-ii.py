0a5356a949854380f9e23feb1a6123a0e4f71953
Saravanan728 <120799881+Saravanan728@users.noreply.github.com>
2026-03-07 20:05:46 +0530

    Time: 8 ms (13.05%) | Memory: 20.6 MB (56.75%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,28 @@
+from  collections import deque
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph=[[] for _ in range(numCourses)]
+        indegree=[0]*numCourses
+        for a,b in prerequisites:
+            graph[b].append(a)
+            indegree[a]+=1
+        q=deque()
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+
+        topo=[]
+        while q:
+            node=q.popleft()
+            topo.append(node)
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        if len(topo)==numCourses:
+            return topo
+        else:
+            return []
+        
