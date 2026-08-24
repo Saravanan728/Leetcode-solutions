91cea7670ee0cb4b49f5a21ba8a5c1d6302b23a9
Saravanan728 <120799881+Saravanan728@users.noreply.github.com>
2026-08-24 23:57:53 +0530

    Time: 4 ms (53.47%) | Memory: 57.3 MB (40.63%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map=new Map<number,number>();
+    for (let i=0;i<nums.length;i++){
+        const comp=target-nums[i];
+        if(map.has(comp)){
+            return[map.get(comp)!,i];
+        }
+        map.set(nums[i],i);
+    }
+    return[];
+    
+};
