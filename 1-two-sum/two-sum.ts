86e8dec7b9dd9d7b41e1f7afacbf83acef82a44d
Saravanan728 <120799881+Saravanan728@users.noreply.github.com>
2026-08-25 00:05:35 +0530

    Time: 4 ms (53.47%) | Memory: 58.6 MB (12.28%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,11 +1,34 @@
 function twoSum(nums: number[], target: number): number[] {
-    const map=new Map<number,number>();
-    for (let i=0;i<nums.length;i++){
-        const comp=target-nums[i];
-        if(map.has(comp)){
-            return[map.get(comp)!,i];
+    // const map=new Map<number,number>();
+    // for (let i=0;i<nums.length;i++){
+    //     const comp=target-nums[i];
+    //     if(map.has(comp)){
+    //         return[map.get(comp)!,i];
+    //     }
+    //     map.set(nums[i],i);
+    // }
+    // return[];
+
+    const arr=nums.map((value,index)=>({value,index}));
+    arr.sort((a,b)=>a.value-b.value);
+
+    let left:number =0;
+    let right:number=nums.length-1;
+
+    while (left<right){
+        const sum=arr[left].value+arr[right].value;
+        if(sum===target){
+            return[arr[left].index,arr[right].index];
+
         }
-        map.set(nums[i],i);
+
+        if(sum<target){
+            left++;
+
+        }else{
+            right--;
+        }
+
     }
     return[];
     
